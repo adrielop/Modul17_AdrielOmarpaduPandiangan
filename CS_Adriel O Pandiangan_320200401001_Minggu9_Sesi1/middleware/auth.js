541e867bfa7e7a532b18dc58f3cc6a6a4a01dbb6
adrielop <92299780+adrielop@users.noreply.github.com>
2022-12-03 14:22:56 +0700

    Update auth.js

--- CS_Adriel O Pandiangan_320200401001_Minggu9_Sesi1/middleware/auth.js
+++ CS_Adriel O Pandiangan_320200401001_Minggu9_Sesi1/middleware/auth.js
@@ -8,7 +8,9 @@ const Auth = {
     //const token = req.cookies['jwt'];
     
     //untuk kebutuhan frontend
-    const {token} = req.body
+    //const {token} = req.body
+    
+    const token = req.body.token
     
     console.log(token)
 
