6502e02fba5bd33fda6e1409557f831ff4566a9a
adrielop <92299780+adrielop@users.noreply.github.com>
2022-12-04 17:27:23 +0700

    Update auth.js

--- CS_Adriel O Pandiangan_320200401001_Minggu9_Sesi1/middleware/auth.js
+++ CS_Adriel O Pandiangan_320200401001_Minggu9_Sesi1/middleware/auth.js
@@ -5,30 +5,20 @@ const config = process.env;
 const Auth = {
   verifyToken(req, res, next) {
     //untuk kebutuhan postman
-    const token = req.cookies['jwt'];
+    const tokenPM = req.cookies['jwt'];
     
     //untuk kebutuhan frontend
-    //const {token} = req.body;
+    const {token} = req.body;
    
    
-    try{
-      if (token) {
+    if (token) {
         // 12. Lalukan jwt verify
         const decode = jwt.verify(token, config.SECRET);
         req.verified = decode
         return next()
         
-      } else {
-        res
-          .status(403)
-          .send({ message: "Youre not authenticated, please login first" });
-        console.log("You are not authenticated");
-      }
-    }catch{
-      const {token}=req.body;
-      if (token) {
-        // 12. Lalukan jwt verify
-        const decode = jwt.verify(token, config.SECRET);
+      } else if(tokenPM){
+        const decode = jwt.verify(tokenPM, config.SECRET);
         req.verified = decode
         return next()
       } else {
@@ -37,7 +27,6 @@ const Auth = {
           .send({ message: "Youre not authenticated, please login first" });
         console.log("You are not authenticated");
       }
-    }
   
   },
 };
